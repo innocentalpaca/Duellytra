2acbc796854d2a848e1acc84f4f84b91a4c1075e
Kilian Kahn <kiliankahn@gmail.com>
2020-08-08 22:00:55 +0200

    Create basic structure

---
+++ src/main/java/me/innocentalpaca/duellytra/Duellytra.java
@@ -0,0 +1,16 @@
+package me.innocentalpaca.duellytra;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Duellytra extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+}
